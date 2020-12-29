7f65842b3b45eeba42f55deb6e03733b4ba1773d
Quarroz Antoine <antoinequarroz.mediam@gmail.com>
2020-12-29 22:40:27 +0100

    Changement de Parcours en competence et premiere section cv

--- src/Controller/CompetenceController.php
+++ src/Controller/CompetenceController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ParcoursController extends AbstractController
+class CompetenceController extends AbstractController
 {
     /**
-     * @Route("/parcours", name="parcours")
+     * @Route("/competence", name="competence")
      */
     public function index(): Response
     {
-        return $this->render('parcours/index.html.twig');
+        return $this->render('competence/index.html.twig');
     }
 }
