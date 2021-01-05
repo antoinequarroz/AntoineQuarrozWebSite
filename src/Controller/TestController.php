d7c6c0b4166c5e8b03ae1cb2ff4cdc09f09001f0
Quarroz Antoine <antoinequarroz.mediam@gmail.com>
2021-01-06 00:40:13 +0100

    Finish

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -13,7 +13,7 @@ class TestController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('test/index.html.twig', [
+        return $this->render('test/indexcv.html.twig', [
             'controller_name' => 'TestController',
         ]);
     }
