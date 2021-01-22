2735cfe571755e830393f197965beb3c2b033e71
Quarroz Antoine <antoinequarroz.mediam@gmail.com>
2021-01-22 15:11:09 +0100

    Ajout de la base de donnée Blog

---
+++ src/Entity/Blog.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BlogRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BlogRepository::class)
+ */
+class Blog
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
