91478ccf98bd6e79245cdbfdf17addeffeb99a96
Quarroz Antoine <antoinequarroz.mediam@gmail.com>
2021-02-23 09:32:02 +0100

    test

---
+++ src/Entity/Projet.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProjetRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProjetRepository::class)
+ */
+class Projet
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
